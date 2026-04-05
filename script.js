931cf24f57ddbed9038d39c42b06ec6fd8c96aba
golesmaryjoy33-code <golesmaryjoy33@gmail.com>
2026-04-05 06:22:37 +0000

    declared variable

---
+++ script.js
@@ -0,0 +1,6 @@
+
+const choices=["rock","paper","scissors"];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay= document.getElementById("computerDisplay");
+const resultDisplay= document.getElementById("resultDisplay");
+
