da3f999af81cb44c082bd5084645478ab9f3e3e8
golesmaryjoy33-code <golesmaryjoy33@gmail.com>
2026-04-11 09:07:19 +0000

    update js

---
+++ script.js
@@ -0,0 +1,16 @@
+let playerscore = 0;
+let omputerscore = 0;
+
+
+function playgame(playerchoice){
+    const choices = ['rock','paper','scissors'];
+    const computerchoices = choice[Math.floor(Math.random() * 3)];
+
+    let resultTExt = "";
+
+    if(playerchoice === computerChoice){
+        resultText= "I"
+
+    }
+    
+}
