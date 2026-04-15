0b37cf7d1708d01799fa8135a34e2d55486bf159
golesmaryjoy33-code <golesmaryjoy33@gmail.com>
2026-04-15 03:31:35 +0000

    edit js

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 let playerscore = 0;
-let omputerscore = 0;
+let computerscore = 0;
 
 
-function playgame(playerchoice){
+function playgame(playerDisplay){
     const choices = ['rock','paper','scissors'];
     const computerchoices = choice[Math.floor(Math.random() * 3)];
 
-    let resultTExt = "";
+    let resultDisplay = "";
 
-    if(playerchoice === computerChoice){
-        resultText= "I"
+    if(playerDisplay === computerDisplay){
+        resultDisplay= "It's a tie!";
 
-    }
+    }else if(
+        (
+            (playerDisplay === 'rock ' && computerDisplay === 'scissors') ||
+            (playerDisplay === 'paper' && computerDisplay === 'rock') ||
+            (playerDisplay === 'scissors' && computerDisplay === 'paper')
     
+        ) 
+            {
+            resultDisplay="You Win!🎉";
+            playerscore++;
+        }else{
+            resultDisplay="You Lose!😔";
+            computerscore++;
+
+        }
+
+
+    document.getElementById('result').innerText= `You chose ${playerDisplay}, Computer chose ${computerDisplay}. ${resultDisplay}`;
+    document.getElementById('score').innerText= `Player: ${playerscore} | Computer: ${computerscore}`;
+
 }
